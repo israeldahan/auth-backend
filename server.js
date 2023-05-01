5435f46301edec3cc0d8b09aa797be87fd1116f8
Israel Dahan <israeldavid.dahan@medtronic.com>
2023-05-01 06:29:13 +0300

    add register and validate

--- server.js
+++ server.js
@@ -10,6 +10,29 @@ app.get('/', (req, res) => {
     res.send({msg: 'Hello World israel'});
 });
 
+function validateInput(username, password, email) {
+    // if username is string length > 3
+    // if password is string and UpperCase and LowerCase and number and length > 6
+    // if email is string and email format
+    let validateInput = false;
+    if (username.length > 3) {
+        validateInput = true;
+    } else {
+        validateInput = false;
+    }
+    if (password.length > 6 && password.match(/[a-z]/g) && password.match(/[A-Z]/g) && password.match(/[0-9]/g)) {
+        validateInput = true;
+    } else {
+        validateInput = false;
+    }
+    if (email.includes('@') && email.includes('.')) {
+        validateInput = true;
+    } else {
+        validateInput = false;
+    }
+    return validateInput;
+}
+
 function auth(req, res, next) {
     const token = req.headers.authorization;
     if (token === '123456789') {
@@ -25,6 +48,15 @@ app.post('/login', (req, res) => {
         res.status(200).send({msg: 'Login Success', token: '123456789'});
     } else {
         res.status(401).send({msg: 'Login Failed'});
+    }
+});
+
+app.post('/register', (req, res) => {
+    const {username, password, email} = req.body;
+    if (validateInput(username, password, email)) {
+        res.status(200).send({msg: 'Register Success'});
+    } else {
+        res.status(401).send({msg: 'Register Failed'});
     }
 });
 
